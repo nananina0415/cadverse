7e81df46a1ed64894b27c948844e2e7aa250b443
minjun4094 <minjun4094@gmail.com>
2025-11-19 19:38:44 +0900

    시뮬인터페이스 템플릿 작성

---
+++ prototype/sim_server/simInterface.py
@@ -0,0 +1,18 @@
+import simulate
+
+class Simulator:
+    def step():
+        ....
+
+class SimState:
+    ...
+
+class SimDescription:
+    simMetaJson = "..."
+
+def make_sim(simDescription: SimDescription)->(Simulator,SimState):
+    simulate.make_sim()
+
+
+
+
