124774f3a6e90e25931afdf778d0d1f6471589ef
nananina <nananina0415@gmail.com>
2025-11-21 12:21:35 +0900

    소유권버퍼 전체읽어올때 커밋락걸도록 수정

--- prototype/sim_server/utils/owned_buffer.py
+++ prototype/sim_server/utils/owned_buffer.py
@@ -7,8 +7,7 @@ class OwnedBuffer:
         self._buff = initialBuff
         self._ownership = threading.Lock()
         self._commitLock = threading.Lock()
-        self.readonly = lambda: copy.deepcopy(self._buff) # 전부 필요할 때
-        self.readRef = lambda key: self._buff[key]        # 하나씩 접근할 때
+
         def commit(newBuff: Indexable):
             with self._commitLock:
                 self._buff = newBuff
@@ -16,8 +15,22 @@ class OwnedBuffer:
 
     def __enter__(self):
         self._ownership.acquire()
-        return (self.commit,self.readRef)
+        return (self.commit,self._readRef)
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._ownership.release()
         return False
+
+    # 전부 필요할 때 사용
+    def readonly(self):
+        cp = None
+        with self._commitLock:
+            cp =  copy.deepcopy(self._buff)
+        return cp
+
+    # 하나씩 접근할 때 사용
+    # 내부 함수임. 밖에서 사용 금지
+    # 만약 오너가 쓰기 중이면 데이터가 깨질 위험 있음
+    # 내부에서도 오너락을 잡은 상태에서 호출돼야 함
+    def _readRef(self, key):
+        return self._buff[key]
