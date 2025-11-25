9bc7c7e307bd5fb58809a4841a3633458ba5b349
nananina <nananina0415@gmail.com>
2025-11-25 16:24:24 +0900

    새 메인 로직에 맞게 루프스레드 개선

--- prototype/sim_server/utils/loop_thread.py
+++ prototype/sim_server/utils/loop_thread.py
@@ -1,37 +1,76 @@
 import threading
 import traceback
+from typing import Callable, TypeVar, Optional
 
-# TODO: 문서화, 타입힌트, 테스트작성, (메트릭로깅&프로파일링)
+T = TypeVar('T')
+
 
 class LoopThread(threading.Thread):
-    def __init__(self, target, args=(), kwargs=None, cleanup=None, daemon=False):
+    """
+    초기화 함수로 객체를 생성하고, 루프 함수를 반복 실행하는 스레드
+
+    사용 예:
+        thread = LoopThread(
+            initFn=lambda: MyObject(),
+            loopFn=lambda obj: obj.step(),
+            clearFn=lambda obj: obj.clear()
+        )
+        thread.start()
+    """
+
+    def __init__(
+        self,
+        initFn: Callable[[], T],
+        loopFn: Callable[[T], None],
+        clearFn: Optional[Callable[[T], None]] = None,
+        daemon: bool = False
+    ):
+        """
+        Args:
+            initFn: 스레드 시작 시 호출되어 객체를 생성하는 함수
+            loopFn: 반복 실행할 함수 (initFn이 반환한 객체를 인자로 받음)
+            clearFn: 스레드 종료 시 정리 함수 (선택적)
+            daemon: 데몬 스레드 여부
+        """
         super().__init__(daemon=daemon)
-        self.target = target
-        self.args = args
-        self.kwargs = kwargs if kwargs is not None else {}
-        self.cleanup = cleanup  # 종료 시 실행할 callback
+        self.initFn = initFn
+        self.loopFn = loopFn
+        self.clearFn = clearFn
         self._stopFlag = threading.Event()
         self._startFlag = threading.Event()
 
     def run(self):
-        self._startFlag.set()
+        """스레드 실행"""
+        obj = None
         try:
+            # 초기화
+            obj = self.initFn()
+            self._startFlag.set()
+
+            # 루프 실행
             while not self._stopFlag.is_set():
-                self.target(*self.args, **self.kwargs)
-        except Exception:
+                self.loopFn(obj)
+
+        except Exception as e:
+            print(f"[LoopThread] 에러 발생: {e}")
             traceback.print_exc()
         finally:
-            if self.cleanup:
+            # 정리
+            if obj is not None and self.clearFn is not None:
                 try:
-                    self.cleanup()
-                except Exception:
+                    self.clearFn(obj)
+                except Exception as e:
+                    print(f"[LoopThread] 정리 중 에러: {e}")
                     traceback.print_exc()
 
     def stop(self):
+        """스레드 중지"""
         self._stopFlag.set()
 
     def waitStartEvent(self):
+        """스레드가 시작될 때까지 대기"""
         self._startFlag.wait()
 
     def wait_stopped(self):
+        """스레드가 완전히 종료될 때까지 대기"""
         self.join()
